173161bae965245c1343046e1106603ef51c010b
ayibogucu <ismail.inal999@gmail.com>
2025-03-01 10:53:10 +0300

    fixed converting to integer issue

--- src/lib/circle_detection.py
+++ src/lib/circle_detection.py
@@ -1,12 +1,12 @@
 from cv2 import imread
 from ultralytics.models.yolo import YOLO
-from numpy import ndarray
+import numpy as np
 from typing import Optional
 
 
 def get_bounding_boxes(
     model: YOLO, img_path: str, buffer_size: int
-) -> Optional[ndarray]:
+) -> Optional[np.ndarray]:
     img = imread(img_path)
     results = model(img)
     bboxes = results[0].boxes.xyxy[:, :4]
@@ -14,9 +14,11 @@ def get_bounding_boxes(
     if bboxes.shape[0] == 0:
         return None
 
+    bboxes = np.round(bboxes.cpu().numpy()).astype(int)
+
     bboxes[:, 0] -= buffer_size
     bboxes[:, 1] -= buffer_size
     bboxes[:, 2] += buffer_size
     bboxes[:, 3] += buffer_size
 
-    return bboxes.cpu().numpy()
+    return bboxes
